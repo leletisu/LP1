637a3eb117d8d67f293101a6fb05b20d3c34298e
Letícia Araujo <162067642+leletisu@users.noreply.github.com>
2025-09-06 16:34:59 -0300

    Create s4-ex1.c

---
+++ Slide 04 - Funções/s4-ex1.c
@@ -0,0 +1,40 @@
+/*Crie uma função eh_primo que recebe um inteiro n e retorna se ele é primo ou não.
+
+Exemplos:
+
+eh_primo(4) -> 0
+eh_primo(7) -> 1*/
+
+#include <stdio.h>
+
+int eh_primo(int n) {
+    if (n <= 1) {
+        return 0;
+    }
+    
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return 0; 
+    }
+    
+    return 1; 
+}
+}
+
+int main() {
+
+    int num;
+
+    printf("Digite um número inteiro positivo: ");
+    scanf("%d", &num);
+
+    eh_primo(num);
+
+    if (num == 1) {
+        printf("%d é um número primo.\n", num);
+    } else {
+        printf("%d não é um número primo.\n", num);
+    }
+
+    return 0;
+}
