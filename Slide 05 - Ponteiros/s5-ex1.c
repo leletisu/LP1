a5e2586bb94af338012ba8d7baace9d9cb5f4a63
Letícia Araujo <162067642+leletisu@users.noreply.github.com>
2025-09-14 22:55:05 -0300

    Update s5-ex1.c

--- Slide 05 - Ponteiros/s5-ex1.c
+++ Slide 05 - Ponteiros/s5-ex1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    int a, b;
+    int *p;
+    
+    printf("Digite dois inteiros: ");
+    scanf("%d %d", &a, &b);
+    
+    printf("Valores iniciais: a = %d, b = %d\n", a, b);
+    
+    if (a > b) {
+        p = &a;
+    } else {
+        p = &b;
+    }
+    
+    *p = *p - 50;
+    
+    printf("Valores finais: a = %d, b = %d\n", a, b);
+    
+    return 0;
+}
