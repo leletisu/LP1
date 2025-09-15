21ef3394cfb5475ff8317ffb7d2c7de50957c7bf
Letícia Araujo <162067642+leletisu@users.noreply.github.com>
2025-09-14 23:30:20 -0300

    Create s5-ex4.c

---
+++ Slide 05 - Ponteiros/s5-ex4.c
@@ -0,0 +1,19 @@
+/*Crie uma função troca que receba dois ponteiros para inteiros p1 e p2 e troque os conteúdos por eles apontados:*/
+
+#include <stdio.h>
+
+void troca(int* p1, int* p2) {
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
+
+int main() {
+    int x = 10, y = 20;
+    
+    printf("Antes da troca: x = %d, y = %d\n", x, y);
+    troca(&x, &y);
+    printf("Depois da troca: x = %d, y = %d\n", x, y);
+    
+    return 0;
+}
