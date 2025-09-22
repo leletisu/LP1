964489cefc9427721a3907e9f8ae851fa94ec84a
Letícia Araujo <162067642+leletisu@users.noreply.github.com>
2025-09-21 22:37:16 -0300

    Update s6-ex1.c

--- slide06-vetores/s6-ex1.c
+++ slide06-vetores/s6-ex1.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+void preenche(int* xs, int n) {
+    for(int i = 0; i < n; i++) {
+        printf("Insira o valor %d: ", i + 1);
+        scanf("%d", xs + i); 
+    }
+}
+
+float media(int* xs, int n) {
+    int soma = 0;
+    for(int i = 0; i < n; i++) {
+        soma += *(xs + i);
+    }
+    return (float)soma / n;
+}
+
+int main(void) {
+    int vec[100];
+    int qnt;
+    
+    printf("Quantidade de elementos do vetor? (Máx 100): ");
+    scanf("%d", &qnt);
+    
+    preenche(vec, qnt);
+    
+    double resultado = media(vec, qnt);
+    printf("A média dos valores é: %.2f\n", resultado);
+    
+    return 0;
+}
