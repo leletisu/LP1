a9291db3450e6962388b8540ebea8c58e3171dc7
Letícia Araujo <162067642+leletisu@users.noreply.github.com>
2025-09-21 22:31:02 -0300

    Update 6-ex1.c

--- slide06-strings/6-ex1.c
+++ slide06-strings/6-ex1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int tamanho(char* str) {
+    int contador = 0;
+    
+    while (str[contador] != '\0') {
+        contador++;
+    }
+
+    return contador;
+}
+
+int main() {
+    char texto1[] = "";
+    char texto2[] = "Hello World!";
+    
+    printf("Tamanho da string vazia: %d\n", tamanho(texto1));  
+    printf("Tamanho de '%s': %d\n", texto2, tamanho(texto2));
+    
+    return 0;
+}
